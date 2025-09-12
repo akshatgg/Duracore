82193f41582d6cba1b9fa85bd0ba9f038034e039
Apoorv2004 <itzmenishu04@gmail.com>
2025-09-12 21:21:31 +0530

    fix contact section

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/contactx" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
           <Route path="/services" element={<Services />} />
           <Route path="/products" element={<Products />} />
           <Route path="/quote" element={<Quote />} />
